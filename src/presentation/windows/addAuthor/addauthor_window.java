f163bc03d7a7f24eb39ed08af7fe7d5e91ac3fa8
agabaivan <agabaivan00@gmail.com>
2023-10-12 17:50:16 -0500

    Added addAuthor window.

--- src/presentation/windows/addAuthor/addauthor_window.java
+++ src/presentation/windows/addAuthor/addauthor_window.java
@@ -1,8 +1,9 @@
+package presentation.windows.addAuthor;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
